f7803a17205d2578ed276b327e8401e33a6ef4a2
nedclimax <maroueneboughoufa@gmail.com>
2024-03-07 13:31:23 +0100

    Added new emacs configuration

---
+++ .emacs.d/lisp/text-edit.el
@@ -0,0 +1,136 @@
+;; NOTE: Most of these functions have been
+;; copied and pasted from https://www.emacswiki.org/emacs/basic-edit-toolkit.el
+
+(defun move-text-internal (arg)
+  "Move region (transient-mark-mode active) or current line."
+  (let ((remember-point (point)))
+    ;; Can't get correct effect of `transpose-lines'
+    ;; when `point-max' is not at beginning of line
+    ;; So fix this bug.
+    (goto-char (point-max))
+    (if (not (bolp)) (newline)) ; add newline to fix
+    (goto-char remember-point)
+    ;; logic code start
+    (cond ((and mark-active transient-mark-mode)
+           (if (> (point) (mark))
+               (exchange-point-and-mark))
+           (let ((column (current-column))
+                 (text (delete-and-extract-region (point) (mark))))
+             (forward-line arg)
+             (move-to-column column t)
+             (set-mark (point))
+             (insert text)
+             (exchange-point-and-mark)
+             (setq deactivate-mark nil)))
+          (t
+           (let ((column (current-column)))
+             (beginning-of-line)
+             (when (or (> arg 0) (not (bobp)))
+               (forward-line 1)
+               (when (or (< arg 0) (not (eobp)))
+                 (transpose-lines arg))
+               (forward-line -1))
+             (move-to-column column t))))))
+
+(defun move-text-up (arg)
+  "Move region (transient-mark-mode active) or current line ARG lines up."
+  (interactive "*p")
+  (move-text-internal (- arg)))
+
+(defun move-text-down (arg)
+  "Move region (transient-mar-mode active) or current line (ARG lines) down."
+  (interactive "*p")
+  (move-text-internal arg))
+
+(defun duplicate-line-or-region-above (&optional reverse)
+  "Duplicate current line or region above.
+   By default, duplicate current line above.
+   If mark is activate, duplicate region lines above.
+   Default duplicate above, unless option REVERSE is non-nil."
+  (interactive)
+  (let ((origianl-column (current-column))
+        duplicate-content)
+    (if mark-active
+        ;; If mark active.
+        (let ((region-start-pos (region-beginning))
+              (region-end-pos (region-end)))
+          ;; Set duplicate start line position.
+          (setq region-start-pos (progn
+                                   (goto-char region-start-pos)
+                                   (line-beginning-position)))
+          ;; Set duplicate end line position.
+          (setq region-end-pos (progn
+                                 (goto-char region-end-pos)
+                                 (line-end-position)))
+          ;; Get duplicate content.
+          (setq duplicate-content (buffer-substring region-start-pos region-end-pos))
+          (if reverse
+              ;; Go to next line after duplicate end position.
+              (progn
+                (goto-char region-end-pos)
+                (forward-line +1))
+            ;; Otherwise go to duplicate start position.
+            (goto-char region-start-pos)))
+      ;; Otherwise set duplicate content equal current line.
+      (setq duplicate-content (buffer-substring
+                               (line-beginning-position)
+                               (line-end-position)))
+      ;; Just move next line when `reverse' is non-nil.
+      (and reverse (forward-line 1))
+      ;; Move to beginning of line.
+      (beginning-of-line))
+    ;; Open one line.
+    (open-line 1)
+    ;; Insert duplicate content and revert column.
+    (insert duplicate-content)
+    (move-to-column origianl-column t)))
+
+(defun duplicate-line-or-region-below ()
+  "Duplicate current line or region below.
+   By default, duplicate current line below.
+   If mark is activate, duplicate region lines below."
+  (interactive)
+  (duplicate-line-or-region-above t))
+
+(defun duplicate-line-above-comment (&optional reverse)
+  "Duplicate current line above, and comment current line."
+  (interactive)
+  (if reverse
+      (duplicate-line-or-region-below)
+    (duplicate-line-or-region-above))
+  (save-excursion
+    (if reverse
+        (forward-line -1)
+      (forward-line +1))
+    (comment-or-uncomment-region+)))
+
+(defun mark-line ()
+  "Mark one whole line, similar to `mark-paragraph'."
+  (interactive)
+  (beginning-of-line)
+  (if mark-active
+      (exchange-point-and-mark)
+    (push-mark nil nil t))
+  (forward-line)
+  (exchange-point-and-mark))
+
+(defun open-newline-above (arg)
+  "move to the previous line (like vi) and then opens a line."
+  (interactive "p")
+  (beginning-of-line)
+  (open-line arg)
+  (if (not (member major-mode '(haskell-mode org-mode literate-haskell-mode)))
+      (indent-according-to-mode)
+    (beginning-of-line)))
+
+(defun open-newline-below (arg)
+  "Move to the next line (like vi) and then opens a line."
+  (interactive "p")
+  (end-of-line)
+  (open-line arg)
+  (call-interactively 'next-line arg)
+  (if (not (member major-mode '(haskell-mode org-mode literate-haskell-mode)))
+      (indent-according-to-mode)
+    (beginning-of-line)))
+
+(provide 'text-edit)
